03c70822871268ba1a695895be594e557f812ad4
Maolei <tangmaoleismile@gmail.com>
2017-08-12 12:02:08 -0700

    add vagrantfile

---
+++ setup_env.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Provision Start"
+apt-get update
+
+echo "Installing libmagickwand-dev"
+apt-get install libmagickwand-dev -y > /dev/null
+
+echo "Installing virtualenv"
+apt-get install python-virtualenv -y > /dev/null
+
+echo "Installing python-dev"
+apt-get install python-dev -y > /dev/null
+
+echo "Preparing MySQL"
+apt-get install debconf-utils -y > /dev/null
+
+debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password development"
+debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password development"
+echo "Installing MySQL"
+apt-get install mysql-server-5.5 -y > /dev/null
+
+apt-get install git -y > /dev/null
+
+echo "Provision End"
