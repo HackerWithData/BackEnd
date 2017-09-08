20b5ff9418bf381b49aeffab8799dd81e0b0a615
hahtml <tangmaoleismile@gmail.com>
2017-09-07 22:12:33 -0700

    complete pagination functionality

--- backend_core/search/views.py
+++ backend_core/search/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect, render_to_response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 # # from forms import BaseSearchForm
 # from forms import TargetTypeSearchForm
@@ -9,36 +10,43 @@ from contractors.models import Contractor
 
 
 def search_new(request):
-    # if request.method == 'POST':
-    #     form = SearchForm(request.POST)
-    #     if form.is_valid():
-    #         pass
-    #
-    # else:
-    #     initial_value = {
-    #         'query_target': 'Designer',
-    #         'query_keywords': 'Interior Design',
-    #         'query_location': '90024',
-    #     }
-    #     form = SearchForm(initial=initial_value)
     if request.method == 'GET':
         # TODO: set default value
         target = request.GET['target']
         # keywords = request.GET['keywords']
         zipcode = request.GET['zipcode']
         query_set = Contractor.objects.filter(PosCode=zipcode).filter(LicType__icontains=target)
+
+        # TODO: set customized item number per page, default = 10
+        # pagination logic
+        paginator = Paginator(query_set, 10)
+        page = request.GET.get('page')
+        try:
+            page_query_set = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            page_query_set = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            page_query_set = paginator.page(paginator.num_pages)
+
+        get_copy = request.GET.copy()
+        parameters = get_copy.pop('page', True) and get_copy.urlencode()
+
+        print parameters
+        # context['constractors'] =
+        # context = {
+        #     'contractors': page_query_set,
+        #     'parameters': parameters
+        # }
     else:
         return redirect('home_index')
-    return render(request, 'search_list/search_list.html', {'contractors': query_set})
+    return render(request, 'search_list/search_list.html', {
+        'contractors': page_query_set,
+        'parameters': parameters
+    })
 
-#
-# def search(request):
-#     # form = BaseSearchForm(request.GET)
-#     form = TargetTypeSearchForm(request.GET)
-#     results = form.search()
-#     return render_to_response('search_bar.html', {'results': results})
-#
-#
+
 def search_dispatch_test(request):
     return HttpResponse('<h1>search dispatch successfully!!!</h1>')
 
