7724a049bd5fa88fed7ee8c7d7ef1213c45ced3a
hahtml <tangmaoleismile@gmail.com>
2017-09-09 17:48:28 -0700

    complete raw search flow

--- backend_core/search/search_helpers.py
+++ backend_core/search/search_helpers.py
@@ -1,2 +1,31 @@
-def convert_str_to_keywords(str):
-    pass
+import urllib
+
+from contractors.models import Contractor
+
+
+# Ajax POST request
+def search_by_address_object(request):
+    zipcode = int(request['address']['address_components'][6]['short_name'])
+    target = request['target']
+    query_set = Contractor.objects.filter(PosCode=zipcode).filter(LicType__icontains=target)
+    return query_set
+
+
+# Ajax POST request
+def search_by_address_object_redirect_url(request):
+    print request
+    zipcode = int(request['address']['address_components'][6]['short_name'])
+    target = request['target']
+    url_parameters = {
+        'zipcode': zipcode,
+        'target': target
+    }
+    return urllib.urlencode(url_parameters)
+
+
+# GET request
+def search_by_zipcode(request):
+    target = request.GET['target']
+    zipcode = request.GET['zipcode']
+    query_set = Contractor.objects.filter(PosCode=zipcode).filter(LicType__icontains=target)
+    return query_set
