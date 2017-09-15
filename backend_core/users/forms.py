6dd0a980b06f80e99451d3d171f9478fb6397e4d
hahtml <tangmaoleismile@gmail.com>
2017-09-15 03:23:32 -0700

    add basic flow for seperate signing up

--- backend_core/users/forms.py
+++ backend_core/users/forms.py
@@ -83,22 +83,6 @@ class ConsumerInfoFillUpForm(forms.Form):
         profile.save()
 
 
-
-"""
-class UserChangeForm(forms.ModelForm):
-    A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = User#get_user_model()
-        fields = ('email', 'password', 'is_active', 'is_admin')
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
-"""
+# TODO: implement professional form
+class ProfessionalInfoFillUpForm(forms.Form):
+    pass
--- backend_core/users/views.py
+++ backend_core/users/views.py
@@ -1,107 +1,79 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.dispatch import receiver
-from allauth.account.signals import user_logged_in
 
-from utils import ROLE_CHOICES, PROFESSIONAL, CONSUMER
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm
-from django.contrib.auth import update_session_auth_hash, login, authenticate
-from social_django.models import UserSocialAuth
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views import View
 
+from forms import ConsumerInfoFillUpForm, ProfessionalInfoFillUpForm
 from utils import *
+
 
 @login_required
 def sign_up_complete_info(request, **kwargs):
     if request.user.role == CONSUMER:
         return render(request, 'before_complete_consumer/before_complete_consumer.html')
+        redirect()
     elif request.user.role == PROFESSIONAL:
         return render(request, 'before_complete_professional/before_complete_professional.html')
     else:
         raise UnexpectedMultipleChoice('Unexpected value for user role')
 
-    # user = kwargs.pop('user')
-    # user.role = user.role.lower().capitalize()
-    #
-    # if user.role == dict(ROLE_CHOICES).get(CONSUMER):
-    #     # consumer
-    #     print '1'
-    #     return render(request, 'before_complete_consumer/before_complete_consumer.html')
-    # elif user.role == dict(ROLE_CHOICES).get(PROFESSIONAL):
-    #     # professional
-    #     print '2'
-    #     pass
-    # else:
-    #     print '3'
-    #     raise UnexpectedMutipleChoice("Unexpected multiple choices for user type")
+
+# TODO: test in the morning
+@method_decorator(login_required, name='dispatch')
+class ConsumerProfileView(View):
+
+    form_class = ConsumerInfoFillUpForm
+    template_name = 'consumer_profile_after_signup/consumer_profile_after_signup.html'
+    initial = {
+        'first_name': 'First Name',
+        'last_name': 'Last Name',
+        'zipcode': '90024',
+        'gender': MALE
+    }
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save(request)
+            # TODO: redirect to home page or user dashboard
+            # return redirect('/success/')
+            return HttpResponse('success')
+
+        return render(request, self.template_name, {'form': form})
 
 
+# TODO: test in the morning
+@method_decorator(login_required, name='dispatch')
+class ProfessionalProfileView(View):
 
-#qwer123456
-# def signup(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             email = form.cleaned_data.get('email')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(email=email, password=raw_password)
-#             login(request, user)
-#             return redirect('home_index')
-#     else:
-#         form = SignUpForm()
-#     return render(request, 'registration/signup.html', {'form': form})
-#
-#
-# @login_required
-# def settings(request):
-#     user = request.user
-#
-#     try:
-#         github_login = user.social_auth.get(provider='github')
-#     except UserSocialAuth.DoesNotExist:
-#         github_login = None
-#     try:
-#         twitter_login = user.social_auth.get(provider='twitter')
-#     except UserSocialAuth.DoesNotExist:
-#         twitter_login = None
-#     try:
-#         facebook_login = user.social_auth.get(provider='facebook')
-#     except UserSocialAuth.DoesNotExist:
-#         facebook_login = None
-#
-#     try:
-#         google_login = user.social_auth.get(provider='google-oauth2')
-#     except UserSocialAuth.DoesNotExist:
-#         google_login = None
-#
-#     can_disconnect = (user.social_auth.count() > 1 or user.has_usable_password())
-#     return render(request, 'registration/settings.html', {
-#         'github_login': github_login,
-#         'twitter_login': twitter_login,
-#         'facebook_login': facebook_login,
-#         'google_login': google_login,
-#         'can_disconnect': can_disconnect
-#     })
-#
-# @login_required
-# def password(request):
-#     if request.user.has_usable_password():
-#         PasswordForm = PasswordChangeForm
-#     else:
-#         PasswordForm = AdminPasswordChangeForm
-#
-#     if request.method == 'POST':
-#         form = PasswordForm(request.user, request.POST)
-#         if form.is_valid():
-#             form.save()
-#             update_session_auth_hash(request, form.user)
-#             messages.success(request, 'Your password was successfully updated!')
-#             return redirect('password')
-#         else:
-#             messages.error(request, 'Please correct the error below.')
-#     else:
-#         form = PasswordForm(request.user)
-#     return render(request, 'users/password.html', {'form': form})
+    form_class = ProfessionalInfoFillUpForm
+    template_name = 'professional_profile_after_signup/professional_profile_after_signup.html'
+    # TODO
+    initial = {
+        'first_name': 'First Name',
+        'last_name': 'Last Name',
+        'zipcode': '90024',
+        'gender': MALE
+    }
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # <process form cleaned data>
+            form.save(request)
+            # TODO: redirect to home page or user dashboard
+            return redirect('/success/')
+
+        return render(request, self.template_name, {'form': form})
