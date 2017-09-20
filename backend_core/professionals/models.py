06f1d4c2507cf8b32f05b824f217982c95451657
hahtml <tangmaoleismile@gmail.com>
2017-09-20 03:05:16 -0700

    add subtype and type in form and zipcode search

--- backend_core/professionals/models.py
+++ backend_core/professionals/models.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+from utils import PROFESSIONAL_TYPE_CHOICES, ARCHITECT, DESIGNER, CONTRACTOR
+
 
 #TODO: please change the type of field
 class Professional(models.Model):
     lic_num = models.CharField(max_length=64)
     name = models.CharField(max_length=64)
     entity_type = models.CharField(max_length=64)
-    type = models.CharField(max_length=128)
+    type = models.CharField(
+        max_length=64,
+        choices=PROFESSIONAL_TYPE_CHOICES
+    )
     state = models.CharField(max_length=64)
     postal_code = models.CharField(max_length=64)
 
+
 class ProfessionalType(models.Model):
-    professional = models.OneToOneField(Professional, primary_key=True, on_delete=models.CASCADE)
+    professional = models.ForeignKey('Professional', on_delete=models.CASCADE)
     type = models.CharField(max_length=64)
     sub_type = models.CharField(max_length=64)
-
-
-
--- backend_core/search/search_helpers.py
+++ backend_core/search/search_helpers.py
@@ -1,14 +1,21 @@
 import urllib
 
 from contractors.models import Contractor
+from designers.models import Designer
+from architects.models import Architect
+from professionals.models import Professional, ProfessionalType
+from professionals.utils import PROFESSIONAL_TYPE_CHOICES, ARCHITECT, DESIGNER, CONTRACTOR
 
 
 # Ajax POST request
 def search_by_address_object(request):
     zipcode = int(request['address']['address_components'][6]['short_name'])
     target = request['target']
-    query_set = Contractor.objects.filter(PosCode=zipcode).filter(LicType__icontains=target)
-    return query_set
+    qs_prof = Professional.objects.filter(postal_code=zipcode)
+    # TODO: add subtype later
+    qs_prof_type = qs_prof.professionaltype_set.filter(type__icontains=target)
+    request['target'] =
+    return qs_prof_type
 
 
 # Ajax POST request
@@ -25,7 +32,27 @@ def search_by_address_object_redirect_url(request):
 
 # GET request
 def search_by_zipcode(request):
-    target = request.GET['target']
+    # TODO: rename target in request and add type
+    professional_subtype = request.GET['target']
+    professional_type = request.GET['type']
     zipcode = request.GET['zipcode']
-    query_set = Contractor.objects.filter(PosCode=zipcode).filter(LicType__icontains=target)
-    return query_set
+    # mediate query set
+    qs_prof = Professional.objects.filter(postal_code=zipcode, type=professional_type)
+    qs_prof_type = qs_prof.professionaltype_set.filter(subtype=professional_subtype)
+    # query set for all qualified professional
+    prof_qs = Professional.objects.filter(id__in=qs_prof_type.values('professional_id'))
+    # retrieve corresponding professional through different table
+    if professional_type.upper() == CONTRACTOR:
+        ret_qs = Contractor.objects.filter(lic_num__in=prof_qs.values('lic_num'))
+    elif professional_type.upper() == ARCHITECT:
+        ret_qs = Architect.objects.filter(lic_num__in=prof_qs.values('lic_num'))
+    elif professional_type.upper() == DESIGNER:
+        ret_qs = Designer.objects.filter(lic_num__in=prof_qs.values('lic_num'))
+    else:
+        raise UndefinedProfessionalType("Error: undefined professional type in search_by_zipcode")
+
+    return ret_qs
+
+
+class UndefinedProfessionalType(Exception):
+    pass
