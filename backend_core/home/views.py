bfb287d777f2106e4784dba0a7cc6695d8abaa82
Jeremy Zheng <jzheng8@u.rochester.edu>
2017-09-25 17:29:22 -0700

    homepage final draft maybe

--- backend_core/home/views.py
+++ backend_core/home/views.py
@@ -2,11 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import os
+
 
 # Create your views here.
 def index(request):
-    context = {
-        'test': 'just for test.',
-        'welcome': 'hello world.'
-    }
-    return render(request, 'home/homepage.html', context)
+    bg_img_path = os.listdir(r'media/background-pic/')
+    info_dict = {"background": bg_img_path}
+    # print bg_img_path
+    return render(request, 'home/home.html', info_dict)
