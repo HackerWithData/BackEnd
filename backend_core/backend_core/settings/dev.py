1e1296b938b07732ddd00b5da82fed512bb91319
hahtml <tangmaoleismile@gmail.com>
2017-09-26 02:03:13 -0700

    added eb package configuration5

--- backend_core/backend_core/settings/dev.py
+++ backend_core/backend_core/settings/dev.py
@@ -36,3 +36,8 @@ DEBUG_TOOLBAR_CONFIG = {
 GOOGLE_API_KEY = 'AIzaSyBcdtc-alvt2fEOMgDk_rmYG03ueIwpurg'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+ALLOWED_HOSTS = [
+    'hoome-dev.us-west-2.elasticbeanstalk.com'
+]
