f19231b63d18cdfbe4fedeb9413adbf03c44d3b0
hahtml <tangmaoleismile@gmail.com>
2017-10-07 06:07:53 -0700

    fix bugs

--- backend_core/home/views.py
+++ backend_core/home/views.py
@@ -8,7 +8,8 @@ import os
 
 # Create your views here.
 def index(request):
-    bg_img_path = os.listdir(os.path.join(settings.BASE_DIR, 'media', 'background-pic'))
+    # it is ok to use absolute path here bacause it is used to get list of image name rather than serve them
+    bg_img_path = os.listdir(os.path.join(settings.BASE_DIR, 'static', 'image', 'background-pic'))
     info_dict = {"background": bg_img_path}
     # print bg_img_path
     return render(request, 'home/home.html', info_dict)
