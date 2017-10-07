5c191a340536abf5259d4353123d288393d6e6c1
hahtml <tangmaoleismile@gmail.com>
2017-10-06 20:35:33 -0700

    change user in createsu

--- backend_core/users/management/commands/createsu.py
+++ backend_core/users/management/commands/createsu.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from users.models import User
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not settings.AUTH_USER_MODEL.objects.filter(username="admin").exists():
-            settings.AUTH_USER_MODEL.objects.create_superuser("admin", "admin@admin.com", "admin2017")
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "admin@admin.com", "admin2017")
