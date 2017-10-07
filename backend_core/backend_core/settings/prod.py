72b10e0dd209b6c38553188b3657c294a0222cc7
hahtml <tangmaoleismile@gmail.com>
2017-10-06 21:57:18 -0700

    add static url

--- backend_core/backend_core/settings/prod.py
+++ backend_core/backend_core/settings/prod.py
@@ -52,7 +52,7 @@ else:
     }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'www', 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'www', 'media'),
+MEDIA_ROOT = os.path.join(BASE_DIR, 'www', 'media')
