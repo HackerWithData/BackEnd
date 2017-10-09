aa646670b0edd267248d89378711ee44fdef197d
hahtml <tangmaoleismile@gmail.com>
2017-10-09 03:16:22 -0700

    fix bug

--- backend_core/backend_core/settings/dev.py
+++ backend_core/backend_core/settings/dev.py
@@ -31,7 +31,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': 'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci'
+            'init_command': 'SET default_storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci'
         }
     }
 }
