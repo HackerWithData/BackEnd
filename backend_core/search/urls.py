3a2cccf5df9edb92ce7c531aa0f526c470c3ad8c
hahtml <tangmaoleismile@gmail.com>
2017-10-10 03:03:55 -0700

    HOOME-46 complete without css style

--- backend_core/search/urls.py
+++ backend_core/search/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^test/$', views.search_dispatch_test, name='search_dispatch_test'),
-    # url(r'^(?P<target>[-\w]+)/(?P<zipcode>[-\w ]+)', views.search_new, name='search_new'),
     url(r'^$', views.search_new, name='search_new'),
-    # url(r'^$', views.search_target_type),
 ]
