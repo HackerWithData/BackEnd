9e64519d6c3cdbbce2ef846541d67cc13b591d87
hahtml <tangmaoleismile@gmail.com>
2017-10-19 20:37:57 -0700

    add js

--- backend_core/static/js/main.js
+++ backend_core/static/js/main.js
@@ -7,6 +7,17 @@ jQuery(document).ready(function ($) {
         }
     );
 
+    $('#primary-menu').click(function () {
+        if ($('.mobile-dropdown-group').hasClass('hide')) {
+            $('.mobile-dropdown-group').removeClass('hide');
+            console.log($('#main-nav ul.nav.navbar-nav').css.height)
+            $('#main-nav ul.nav.navbar-nav').css('height',($('#main-nav ul.nav.navbar-nav').height()+80)) ;
+        } else {
+            $('.mobile-dropdown-group').addClass('hide');
+            $('#main-nav ul.nav.navbar-nav').css('height',($('#main-nav ul.nav.navbar-nav').height()-80)) ;
+        }
+    });
+
 
     $('.counter').each(function () {
         var $this = $(this),
