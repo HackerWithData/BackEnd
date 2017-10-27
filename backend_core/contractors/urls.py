6a30e49f3b7d67bfeb687413469b4fed345d6a63
Jeremy Zheng <jzheng8@u.rochester.edu>
2017-10-26 18:05:38 -0700

    rewrite contractor

--- backend_core/contractors/urls.py
+++ backend_core/contractors/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from views import display_contractor, upload_project_photos, display_project_photos
+from views import ContractorDetail, upload_project_photos, display_project_photos
 from review.views import submit_review, display_review
 from photos.views import background_photo_upload
 
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^([0-9]{0,8})/project-photos/upload$', upload_project_photos, name='contractor_project_photos_upload'),
     url(r'^([0-9]{0,8})/background-upload$', background_photo_upload, name='contractor_background_photos_upload'),
     url(r'^([0-9]{0,8})/review/submit$', submit_review, name='contractor_review_submit'),
-    url(r'^([0-9]{0,8})/$', display_contractor, name='contractor'),
+    url(r'^([0-9]{0,8})/$', ContractorDetail.as_view(), name='contractor'),
 ]
