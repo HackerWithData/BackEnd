fab06a48128637dc88b37b6afd03dff0c2d14d14
hahtml <tangmaoleismile@gmail.com>
2017-11-03 13:11:49 -0700

    HOOME-18 edit migration files

--- backend_core/backend_core/settings/dev.py
+++ backend_core/backend_core/settings/dev.py
@@ -15,26 +15,26 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    }
-}
-#
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'hoome_local',
-#         'USER': 'root',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', default_storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci"
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
 #     }
 # }
+#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hoome_local',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', default_storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci"
+        }
+    }
+}
 #
 # DATABASES = {
 #     'default': {
@@ -42,7 +42,7 @@ DATABASES = {
 #         'NAME': 'ebdb',
 #         'USER': 'hoome',
 #         'PASSWORD': 'hoome2017',
-#         'HOST': 'aa16jc2aqeekij9.cpqxzirsz2nd.us-west-2.rds.amazonaws.com',
+#         'HOST': 'aapdhi001fps2q.cpqxzirsz2nd.us-west-2.rds.amazonaws.com',
 #         'PORT': '3306',
 #         'OPTIONS': {
 #             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', default_storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci"
@@ -50,11 +50,8 @@ DATABASES = {
 #     }
 # }
 
-
-
 # TODO: During development only, change SMTP email service properties in prod setting
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 
 # Debug toolbar tools
 DEV_APPS = [
