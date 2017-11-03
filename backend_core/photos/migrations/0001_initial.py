980087f53df1f4b4a42ec7e616d0e84ca7451d35
hahtml <tangmaoleismile@gmail.com>
2017-11-03 13:11:49 -0700

    HOOME-18 edit migration files

--- backend_core/photos/migrations/0001_initial.py
+++ backend_core/photos/migrations/0001_initial.py
@@ -31,6 +31,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('img', models.ImageField(upload_to='photos/photos/%Y/%m/%d')),
+                ('uploaded_at', models.DateTimeField(auto_now_add=True)),
                 ('title', models.CharField(blank=True, max_length=255)),
                 ('object_id', models.PositiveIntegerField()),
                 ('content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.ContentType')),
