2b133424ea23c2e5a11ad434999504041bba0154
Jeremy Zheng <jzheng8@u.rochester.edu>
2017-11-04 17:37:37 -0700

    remove all migrations and create new ones

--- backend_core/projects/migrations/0002_auto_20171105_0035.py
+++ backend_core/projects/migrations/0002_auto_20171105_0035.py
@@ -12,14 +12,18 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('review', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('projects', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='review',
+            model_name='project',
             name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.DO_NOTHING, to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AlterUniqueTogether(
+            name='project',
+            unique_together=set([('project_id', 'content_type', 'object_id', 'user')]),
         ),
     ]
