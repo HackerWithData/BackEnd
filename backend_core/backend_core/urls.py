700336086c90fef0cf9866b3a5cebadf69302f1f
Ubuntu <jeremyzheng@hoome.io>
2017-11-13 05:51:53 +0000

    add files

--- backend_core/backend_core/urls.py
+++ backend_core/backend_core/urls.py
@@ -77,4 +77,5 @@ if settings.DEBUG:
 
     urlpatterns = [
                       url(r'^__debug__/', include(debug_toolbar.urls)),
-                  ] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  ] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+                  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
