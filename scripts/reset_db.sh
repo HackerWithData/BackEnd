8bc03b7f54c7f1a34394c90a4f23c846ca5cd96b
boyang an <boyangan@boyangs-MacBook-Pro.local>
2017-12-02 22:44:02 -0500

    Designer UI: add remove photo function

--- scripts/reset_db.sh
+++
@@ -1,6 +0,0 @@
-echo "Reset database"
-find ./backend_core -path "*/db.sqlite3" -delete
-find ./backend_core -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find ./backend_core -path "*/migrations/*.pyc"  -delete
-python ./backend_core/manage.py makemigrations
-python ./backend_core/manage.py migrate
